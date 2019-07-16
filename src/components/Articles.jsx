610ef6999d81e489f27857d122d2ac3342f7d715
Michael8384-1 <michaellynchsmith.1984@hotmail.co.uk>
2019-07-16 15:47:49 +0100

    added loading to state

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -3,7 +3,8 @@ import * as api from "./api";
 
 class Articles extends Component {
   state = {
-    articles: []
+    articles: [],
+    loading: true
   };
 
   componentDidMount() {
@@ -12,27 +13,33 @@ class Articles extends Component {
 
   getArticleList = () => {
     api.fetchArticles().then(articlesData => {
-      console.log(articlesData);
-      this.setState({ articles: articlesData });
+      this.setState({ articles: articlesData, loading: false });
     });
   };
 
   render() {
     const { articles } = this.state;
+    const { loading } = this.state;
+    let content;
 
-    return articles.map(articleList => {
-      return (
-        <ul key="articles">
-          <li key={articleList.article_id} className="articlesList">
-            <h2>Author:{articleList.author}</h2> <br />
-            <b>Title</b>:{articleList.title}
-            <br />
-            <b>Topic:</b>
-            {articleList.topic}
-          </li>
-        </ul>
-      );
-    });
+    if (loading) {
+      content = <div>Loading...</div>;
+    } else {
+      return articles.map(articleList => {
+        return (
+          <ul className="articles">
+            <li key={articleList.article_id} className="articlesList">
+              <h2 className="author">Author:{articleList.author}</h2> <br />
+              <h3 className="title">Title:{articleList.title}</h3>
+              <br />
+              <h3 className="topic">Topic:{articleList.topic}</h3>
+              <button>Comments</button>
+            </li>
+          </ul>
+        );
+      });
+    }
+    return <div>{content}</div>;
   }
 }
 
