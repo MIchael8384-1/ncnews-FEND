9b235598c2ab1b5ebf9a30d6b810d630188d6220
Michael8384-1 <michaellynchsmith.1984@hotmail.co.uk>
2019-08-05 21:02:22 +0100

    home page with welcome text

---
+++ src/components/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div className="HomePage">
+      <p>Welcome To NC Portal</p>
+    </div>
+  );
+};
+
+export default HomePage;
