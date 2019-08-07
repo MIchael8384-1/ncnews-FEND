4f00a632bfdd95d28cf6388dd2b81d94d337d400
Michael8384-1 <michaellynchsmith.1984@hotmail.co.uk>
2019-08-07 16:50:56 +0100

    added disabled to button

--- src/components/Voter.jsx
+++ src/components/Voter.jsx
@@ -1,19 +1,31 @@
 import React from "react";
-// import * as api from "./api";
+import * as api from "./api";
 
-const Voter = ({ article_id, votes }) => {
-  const vote = increment => {
-    // api.patchArticleVotes(article_id, increment);
-    console.log(increment);
+class Voter extends React.Component {
+  state = {
+    voteChange: 0
   };
+  render() {
+    const { votes } = this.props;
+    const { voteChange } = this.state;
 
-  return (
-    <>
-      <button onClick={() => vote(1)}>LIKE</button>
-      <p>Votes:{votes}</p>
-      <button onClick={() => vote(-1)}>DISLIKE</button>
-    </>
-  );
-};
+    return (
+      <>
+        <button onClick={() => this.vote(1)} disabled={voteChange >= 1}>
+          LIKE
+        </button>
+        <p>Votes:{votes + voteChange}</p>
+        <button onClick={() => this.vote(-1)}>DISLIKE</button>
+      </>
+    );
+  }
+  vote = inc_votes => {
+    const { article_id } = this.props;
+    api.patchArticleVotes(article_id, inc_votes);
+    this.setState(currentState => {
+      return { voteChange: currentState.voteChange + inc_votes };
+    });
+  };
+}
 
 export default Voter;
