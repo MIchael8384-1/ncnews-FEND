423e31487dc485a5acf30972d66eacf8a8ebc412
Michael8384-1 <michaellynchsmith.1984@hotmail.co.uk>
2019-08-08 12:20:56 +0100

    post request with setstate updated with the new comment

--- src/components/CommentAdder.jsx
+++ src/components/CommentAdder.jsx
@@ -1,34 +1,38 @@
 import React, { Component } from "react";
 
 class CommentAdder extends Component {
-  state = { name: "", info: "" };
+  state = { username: "", body: "" };
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
-        <label htmlFor="name">Name:</label>
+        <label htmlFor="username">Name:</label>
         <input
           type="text"
-          view={this.state.name}
-          id="name"
-          onChange={e => this.handleChange(e.target.value, "name")}
+          view={this.state.username}
+          id="username"
+          onChange={e => this.updateInputState(e.target.value, "username")}
         />
-        <label htmlFor="info">Comment:</label>
+        <label htmlFor="body">Comment:</label>
         <input
           type="text"
-          value={this.state.info}
+          value={this.state.body}
           id="info"
-          onChange={e => this.handleChange(e.target.value, "info")}
+          onChange={e => this.updateInputState(e.target.value, "body")}
         />
         <button>Add Comment</button>
       </form>
     );
   }
-  handleChange = (text, key) => {
+  updateInputState = (text, key) => {
     this.setState({ [key]: text });
   };
 
   handleSubmit = e => {
     e.preventDefault();
+    const { username, body } = this.state;
+    console.log(this.props);
+    this.props.addItem({ username, body });
+    this.setState({ username: "", body: "" });
   };
 }
 
