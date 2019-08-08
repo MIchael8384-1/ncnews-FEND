ca42f8d68d9012fb32f2971c6e0517d20cd71bbe
Michael8384-1 <michaellynchsmith.1984@hotmail.co.uk>
2019-08-08 12:18:27 +0100

    functionality added to update comments state with new comment

--- src/components/ArticleComments.jsx
+++ src/components/ArticleComments.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import CommentCards from "./CommentCards";
 import * as api from "./api";
+import CommentAdder from "./CommentAdder";
 
 class ArticleComments extends Component {
   state = { comments: [], isLoading: true };
@@ -8,18 +9,33 @@ class ArticleComments extends Component {
     const { comments, isLoading } = this.state;
     if (isLoading) return <p>Loading</p>;
 
-    return comments.map(commentData => {
-      return (
-        <CommentCards
-          key={commentData.comment_id}
-          author={commentData.author}
-          body={commentData.body}
-          created={commentData.created_at}
-          votes={commentData.votes}
-        />
-      );
-    });
+    return (
+      <div>
+        <CommentAdder addItem={this.addItem} />
+        {comments.map(commentData => {
+          return (
+            <CommentCards
+              key={commentData.comment_id}
+              author={commentData.author}
+              body={commentData.body}
+              created={commentData.created_at}
+              votes={commentData.votes}
+            />
+          );
+        })}
+      </div>
+    );
   }
+
+  addItem = newItem => {
+    console.log(this.props);
+    api.postItem(this.props.article_id, newItem).then(newComment => {
+      this.setState(previousState => {
+        console.log(newComment);
+        return { comments: [newComment, ...previousState.comments] };
+      });
+    });
+  };
 
   getCommentsList = () => {
     api.fetchComments(this.props.article_id).then(commentsData => {
