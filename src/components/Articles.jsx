dd28509a22ca504592296c35e1579edc426f3800
Michael8384-1 <michaellynchsmith.1984@hotmail.co.uk>
2019-08-09 12:41:06 +0100

    sortform component added as well as addtional component did update conditions

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -1,51 +1,65 @@
 import React, { Component } from "react";
 import * as api from "./api";
 import ArticlesCard from "./ArticlesCard";
+import SortForm from "./SortForm";
 
 class Articles extends Component {
   state = {
     articles: [],
+    sort_by: null,
+    order: null,
     loading: true
-  };
-
-  getArticleList = () => {
-    const { topic } = this.props;
-    api.fetchArticles({ topic }).then(articlesData => {
-      this.setState({ articles: articlesData, loading: false });
-    });
   };
 
   render() {
     const { articles, loading } = this.state;
+    if (loading) return <p>Loading...</p>;
 
-    let content;
-
-    if (loading) {
-      content = <div>Loading...</div>;
-    } else {
-      return articles.map(articlesList => {
-        return (
-          <ArticlesCard
-            key={articlesList.article_id}
-            author={articlesList.author}
-            title={articlesList.title}
-            topic={articlesList.topic}
-            id={articlesList.article_id}
-          />
-        );
-      });
-    }
-    return <div>{content}</div>;
+    return (
+      <div>
+        <SortForm sortByOptions={this.sortByOptions} />
+        {articles.map(articlesList => {
+          return (
+            <ArticlesCard
+              key={articlesList.article_id}
+              author={articlesList.author}
+              title={articlesList.title}
+              topic={articlesList.topic}
+              id={articlesList.article_id}
+            />
+          );
+        })}
+      </div>
+    );
   }
+
+  getArticleList = () => {
+    const { topic } = this.props;
+    const { order, sort_by } = this.state;
+    console.log(order);
+    api.fetchArticles({ topic, order, sort_by }).then(articlesData => {
+      this.setState({ articles: articlesData, loading: false });
+    });
+  };
+
   componentDidMount() {
     this.getArticleList();
   }
-  componentDidUpdate(prevProps) {
-    if (prevProps.topic !== this.props.topic) {
+  componentDidUpdate(prevProps, prevState) {
+    if (
+      prevProps.topic !== this.props.topic ||
+      prevState.sort_by !== this.state.sort_by ||
+      prevState.order !== this.state.order
+    ) {
       this.getArticleList();
       // create some additional conditions depending for //sort ||
     }
   }
-}
 
+  sortByOptions = (sort, order_by) => {
+    console.log(sort);
+    console.log(order_by);
+    this.setState({ sort_by: sort, order: order_by });
+  };
+}
 export default Articles;
