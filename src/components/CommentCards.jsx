44623c1323c36ad21365e0d87a3f663928e92fb3
Michael8384-1 <michaellynchsmith.1984@hotmail.co.uk>
2019-08-09 14:32:45 +0100

    delete button added

--- src/components/CommentCards.jsx
+++ src/components/CommentCards.jsx
@@ -7,6 +7,7 @@ const CommentCards = props => {
         <h3>Author:{props.author}</h3>
         <p>{props.body}</p>
         <p>Votes:{props.votes}</p>
+        <button onClick={props.deleteComment}>Delete</button>
       </div>
     </article>
   );
