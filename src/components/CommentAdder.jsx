ffc562c8bf7c9e5658ed118f959c0467c3605247
Michael8384-1 <michaellynchsmith.1984@hotmail.co.uk>
2019-08-11 16:05:12 +0100

    trying to put a condition in place to prevent non users making comments

--- src/components/CommentAdder.jsx
+++ src/components/CommentAdder.jsx
@@ -1,26 +1,31 @@
 import React, { Component } from "react";
 
 class CommentAdder extends Component {
-  state = { username: "", body: "" };
+  state = { username: "jessjelly", body: "" };
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <label htmlFor="username">Name:</label>
+      <div>
+        {this.props.user && (
+          <form onSubmit={this.handleSubmit}>
+            {/* <label htmlFor="username">Name:</label>
         <input
           type="text"
           view={this.state.username}
           id="username"
           onChange={e => this.updateInputState(e.target.value, "username")}
         />
-        <label htmlFor="body">Comment:</label>
-        <input
-          type="text"
-          value={this.state.body}
-          id="info"
-          onChange={e => this.updateInputState(e.target.value, "body")}
-        />
-        <button>Add Comment</button>
-      </form>
+        <label htmlFor="body">Comment:</label> */}
+            <input
+              type="text"
+              value={this.state.body}
+              id="info"
+              required
+              onChange={e => this.updateInputState(e.target.value, "body")}
+            />
+            <button>Add Comment</button>
+          </form>
+        )}
+      </div>
     );
   }
   updateInputState = (text, key) => {
