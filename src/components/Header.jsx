a4a217b175e36779e0f60c51d2b3dde2b3005bc8
Michael8384-1 <michaellynchsmith.1984@hotmail.co.uk>
2019-08-12 14:15:36 +0100

    image added and made responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import HeaderImage from "../../src/NC_Logo2_header.png";
+import "./header.css";
 
 const Header = () => {
   return (
     <section className="header">
-      <img src={HeaderImage} alt="Northcoders Header" class="responsive" />
+      <img src={HeaderImage} alt="Northcoders Header" className="responsive" />
     </section>
   );
 };
