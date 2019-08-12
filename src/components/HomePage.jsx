3dc798c34164693cdce497130a78405e82dff9ac
Michael8384-1 <michaellynchsmith.1984@hotmail.co.uk>
2019-08-12 13:16:41 +0100

    image add to homepage

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,16 +1,13 @@
 import React from "react";
 import "./homePage.css";
+import HomePageLogo from "../../src/NC_Logo_homepage.png";
 
 const HomePage = () => {
   return (
     <div className="homePage">
       <h2>Welcome To NC Portal</h2>
       <p>PLEASE SELECT FROM THE ABOVE OPTIONS</p>
-      <img
-        src="../src/images/NC_Logo_Intro.png"
-        alt="NC logo"
-        className="responsive"
-      />
+      <img src={HomePageLogo} alt="NC logo" className="responsive" />
     </div>
   );
 };
