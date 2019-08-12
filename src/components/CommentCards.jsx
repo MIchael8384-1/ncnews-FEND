43ca47809ebdab4166bd9984201a9b1891767bcf
Michael8384-1 <michaellynchsmith.1984@hotmail.co.uk>
2019-08-12 10:55:58 +0100

    added a condition to my button to prevent anyone else except logged in user to delete own comments

--- src/components/CommentCards.jsx
+++ src/components/CommentCards.jsx
@@ -1,14 +1,21 @@
 import React from "react";
 import VoteOnComments from "./VoteOnComments";
+import "./commentCards.css";
 
 const CommentCards = props => {
+  console.log(props.user);
   return (
     <article className="commentCard">
       <div className="container">
         <h3>Author:{props.author}</h3>
         <p>{props.body}</p>
         <VoteOnComments comment_id={props.comment_id} votes={props.votes} />
-        <button onClick={props.deleteComment}>Delete</button>
+        <br />
+        {props.user === props.author && (
+          <button className="btn-group" onClick={props.deleteComment}>
+            DELETE
+          </button>
+        )}
       </div>
     </article>
   );
