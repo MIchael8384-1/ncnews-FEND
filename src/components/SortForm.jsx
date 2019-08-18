2e40a1d8b13332639aa9cc9dcc168383a1618012
Michael8384-1 <michaellynchsmith.1984@hotmail.co.uk>
2019-08-18 17:14:50 +0100

    adding styling for sortform

--- src/components/SortForm.jsx
+++ src/components/SortForm.jsx
@@ -10,70 +10,76 @@ class SortForm extends React.Component {
     const { sort_by, order } = this.state;
     return (
       <form className="sortForm" onSubmit={this.handleSubmit}>
-        <div className="checkbox">
-          <label className="containerCheckBox">
-            <input
-              type="checkbox"
-              name="sort"
-              value="votes"
-              checked={sort_by === "votes"}
-              onChange={this.handleOptionChange}
-            />
-            VOTES
-          </label>
-        </div>
-        <div className="radio">
-          <label className="containerCheckBox">
-            <input
-              type="checkbox"
-              name="sort"
-              value={"comment_count"}
-              checked={sort_by === "comment_count"}
-              onChange={this.handleOptionChange}
-            />
-            COMMENT COUNT
-          </label>
-        </div>
-        <div className="checkbox">
-          <label className="containerCheckBox">
-            <input
-              type="checkbox"
-              name="sort"
-              value={"created_at"}
-              checked={sort_by === "created_at"}
-              onChange={this.handleOptionChange}
-            />
-            CREATED AT
-          </label>
-        </div>
+        <div className="flex-container">
+          <div className="sortType">
+            <div className="checkbox">
+              <label className="containerCheckBox">
+                <input
+                  type="checkbox"
+                  name="sort"
+                  value="votes"
+                  checked={sort_by === "votes"}
+                  onChange={this.handleOptionChange}
+                />
+                VOTES
+              </label>
+            </div>
+            <div className="checkbox">
+              <label className="containerCheckBox">
+                <input
+                  type="checkbox"
+                  name="sort"
+                  value={"comment_count"}
+                  checked={sort_by === "comment_count"}
+                  onChange={this.handleOptionChange}
+                />
+                COMMENT COUNT
+              </label>
+            </div>
+            <div className="checkbox">
+              <label className="containerCheckBox">
+                <input
+                  type="checkbox"
+                  name="sort"
+                  value={"created_at"}
+                  checked={sort_by === "created_at"}
+                  onChange={this.handleOptionChange}
+                />
+                CREATED AT
+              </label>
+            </div>
+          </div>
 
-        <div>
-          <label className="containerCheckBox">
-            <input
-              type="checkbox"
-              name="order"
-              value="asc"
-              checked={order === "asc"}
-              onChange={this.handleOrderChange}
-            />
-            ASC
-          </label>
-        </div>
-        <div>
-          <label className="containerCheckBox">
-            <input
-              type="checkbox"
-              name="order"
-              value="desc"
-              checked={order === "desc"}
-              onChange={this.handleOrderChange}
-            />
-            DESC
-          </label>
-          <button className="btn-group" type="submit">
-            SORT
-          </button>
-        </div>
+          <div className="sortOrder">
+            <div>
+              <label className="containerCheckBox">
+                <input
+                  type="checkbox"
+                  name="order"
+                  value="asc"
+                  checked={order === "asc"}
+                  onChange={this.handleOrderChange}
+                />
+                ASC
+              </label>
+            </div>
+            <div>
+              <label className="containerCheckBox">
+                <input
+                  type="checkbox"
+                  name="order"
+                  value="desc"
+                  checked={order === "desc"}
+                  onChange={this.handleOrderChange}
+                />
+                DESC
+              </label>
+            </div>
+          </div>
+        </div>{" "}
+        <button className="btn-sort" type="submit">
+          SORT
+        </button>
       </form>
     );
   }
